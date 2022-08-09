db4d6072b7cef3e8f3cc3d2b54a0d0fe77454c4f
luejenie <luejenie@gmail.com>
2022-08-09 15:40:29 +0900

    20220809 조건문(if)

--- 03_control/src/edu/kh/control/condition/ConditionExample.java
+++ 03_control/src/edu/kh/control/condition/ConditionExample.java
@@ -82,5 +82,158 @@ public class ConditionExample {
 	
 	
 	
+	public void ex3() {
+		
+		// if - else if - else
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("1~12 사이의 정수를 입력해주세요 : ");
+		int input = sc.nextInt();
+		
+		
+		String season;  // 변수 선언
+		
+		//봄이 아니라면 여름, 가을, 겨울 중 아무거나 나오게 하려면?
+		if(input >= 3 && input <= 5) {
+			season = "봄";
+			
+		} else if (input >= 6 && input <= 8){
+			season = "여름";
+			
+		} else if (input >= 9 && input <= 11) {
+			season = "가을";
+		
+		} else if (input == 12 || input == 1 || input == 2) {
+			season = "겨울";	
+		
+		} else {
+			season = "해당하는 계절이 없습니다.";
+		}
+		
+				
+		System.out.println(season);   
+		// The local variable season may not have been initialized. 초기화되지 않을 수 있다.
+	}
+	
+		
+		
+	public void ex4() {
+			// 나이를 입력 받아 
+			// 13세 이하면 "어린이"
+			// 13세 초과, 19세 이하 "청소년"
+			// 19세 초과 시 "성인"
+			
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("나이 입력 : ");
+		int age = sc.nextInt();
+		
+		String result;
+		
+		
+		if (age <= 13) {
+			result = "어린이";
+	
+		} else if (age <= 19) {  
+			//else if (input > 13 && input <= 19) 
+			// 이미 위에 조건에서 13미만이 아니라는 걸 알았기 떄문에 13초과 부분이 생략 가능. 그래프 그려보기.
+			// 좀 더 효율적인 코드!
+			result = "청소년";
+			
+		} else {
+			result = "성인";
+		}
+		
+		System.out.println(result);
+				
+	}
+	
+	
+	//다시해보기!
+	public void ex5() {
+		// 놀이기구 탑승 제한 검사
+	      
+	      // 나이가 12세 이상, 키 140.0cm 이상 일 경우에만 "탑승 가능"
+	      // 나이가 12미만인 경우 : "적정 연령이 아닙니다."
+	      // 키가 140.0cm 미만 : "적정 키가 아닙니다."
+	      // 나이를 0세 미만, 100세 초과 시 : "잘못 입력 하셨습니다."
+		
+				// 나이 먼저 생각해보기! -> 잘못된 경우부터 생각하는 게 좋음.
+				// 나이를 0세 미만, 100세 초과부터 생각해 보자.
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("나이 입력 : ");
+		int age = sc.nextInt();
+		
+		System.out.print("키 입력 : ");
+		double height = sc.nextDouble();
+		
+		String result;
+		
+		// 나이 입력 후 키 입력 안하고 바로 "적정 연령이 아닙니다" 나오게 하는 법??
+		
+		//첫 번째 방법. 중첩 if 이용
+		/*  if(age < 0 || age > 100) {  //0세 미만 100세 초과
+			result = "잘못 입력 하셨습니다.";		
+			
+		} else {
+			
+			if(age < 12) { //12세 미만
+				result = "적정 연령이 아닙니다.";
+				
+			} else { //12세 이상
+				
+				if(height < 140.0) { //140 미만
+					result = "적정 키가 아닙니다.";
+				} else { //12세 이상, 140 이상
+					result = "탑승 가능";
+				}
+			}
+		}  */
+		
+		
+		
+		//두 번째 방법. else if 이용
+		
+		if(age < 0 || age > 100) {
+			result = "잘못 입력 하셨습니다.";
+			
+		} else if (age < 12) {   // 이 부분부터 범위가 0 이상 100 이하로 한정되어 짐.
+			result = "적정 연령이 아닙니다.";
+			
+		} else if (height < 140.0) {
+			result = "적정 키가 아닙니다.";
+		
+		} else {
+			result = "탑승 가능";
+		}
+		
+		
+		System.out.println(result);
+	
+	}	
+	
+	
+	
+//		if(age >= 12 && height >= 140.0) {
+//			result = "탑승 가능";			
+//		
+//		} else if(age < 0 || age > 100) {
+//			result = "잘못 입력 하셨습니다.";
+//		
+//		} else if(age < 12) {
+//			result = "적정 연령이 아닙니다.";
+//							
+//		} else {
+//			result = "적정 키가 아닙니다.";
+//	    }
+//		
+//		System.out.println(result);
+	
+	
+	
+
+	
 	
 }
--- 03_control/src/edu/kh/control/condition/ConditionRun.java
+++ 03_control/src/edu/kh/control/condition/ConditionRun.java
@@ -1,5 +1,6 @@
 package edu.kh.control.condition;
 
+
 //코드를 실행시키는 부분(시키는 사람) _Run파트 a.k.a.관리자
 public class ConditionRun {
 	
@@ -9,12 +10,30 @@ public class ConditionRun {
 		ConditionExample ex = new ConditionExample();
 		
 		//커서 놓고 + ctrl + /  : 한 줄 주석
-		//ex. 치면 ex가 할 수 있는 목록이 보임
-//		ex.test1();
+		//ex. 치면 ex가 할 수 있는 목록이 보임  
+//		ex.test1();  //ex에게 test1를 수행해달라고 시키는것
 //		ex.test2();
 		
 //		ex.ex1();
-		ex.ex2();
+//		ex.ex2();
+//		ex.ex3();
+//		ex.ex4();
+//		ex.ex5();
+		
+		
+		//switch 고용하기
+		SwitchExample switchEx = new SwitchExample();
+//		switchEx.ex1();
+//		switchEx.ex2();
+//		switchEx.ex3();
+//		switchEx.ex4();
+		switchEx.ex5();
+		
+		
+		
+		
+		
+		
 		
 		
 		
