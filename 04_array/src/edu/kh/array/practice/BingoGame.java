d4e8fd6e2c58c69c9ecc8e1636bb967ae98249b2
2silverG <luejenie@gmail.com>
2022-08-22 03:25:39 +0900

    20220821 배열 실습문제
    빙고게임

---
+++ 04_array/src/edu/kh/array/practice/BingoGame.java
@@ -0,0 +1,71 @@
+package edu.kh.array.practice;
+
+import java.util.Scanner;
+
+public class BingoGame {
+	
+	public void bingo() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("빙고판 크기 지정 : ");
+		int size = sc.nextInt();
+		
+		int[][] arr = new int[size][size];  //빙고판
+		
+		for(int row=0 ; row<arr.length ; row++) {
+			for(int col=0; col<arr[row].length ; col++) {
+				
+				int ran = (int)(Math.random() * (size*size)+1);   //중복값 제거 해야하나?
+				arr[row][col] = ran;   //정수 난수 무작위 배치	
+				System.out.printf("%5d" , arr[row][col]);
+			}
+			System.out.println();
+		}
+		
+		
+		
+		System.out.println("=========빙고게임 시작=========");
+	
+
+		while(true) {
+		
+			System.out.print("정수를 입력하시오 : ");
+			int num = sc.nextInt();
+			//int count = 0;
+			
+			
+			for(int row=0 ; row<arr.length ; row++) {
+				for(int col=0; col<arr[row].length ; col++) {
+					
+					
+					if(arr[row][col] == num) {
+						
+						arr[row][col] = 0;   //일단 별 대신 0을 찍는다고 하고..
+						//count++;
+					
+					} 
+//					else {
+//						System.out.println("다시 입력해주세요.");
+//						break;
+//					}
+					
+					System.out.printf("%5d" , arr[row][col]);
+				}
+				
+				System.out.println();
+				
+			}
+
+			//System.out.println("현재 " + count + "빙고");  //
+				
+		}
+	
+		
+		
+		
+	}
+	
+	
+
+}
---
+++ 04_array/src/edu/kh/array/practice/BingoGameRun.java
@@ -0,0 +1,13 @@
+package edu.kh.array.practice;
+
+public class BingoGameRun {
+	public static void main(String[] args) {
+		
+		BingoGame bg = new BingoGame();
+		
+		bg.bingo();
+				
+		
+	}
+
+}
