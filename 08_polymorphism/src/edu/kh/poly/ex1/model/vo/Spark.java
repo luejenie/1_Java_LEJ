b1035cf9b6efbdd62f2b6f0c215ce077f7f87af5
luejenie <luejenie@gmail.com>
2022-08-23 17:51:09 +0900

    20220823 다형성

---
+++ 08_polymorphism/src/edu/kh/poly/ex1/model/vo/Spark.java
@@ -0,0 +1,36 @@
+package edu.kh.poly.ex1.model.vo;
+
+public class Spark extends Car {
+	
+	private double discountOffer;  // 할인 혜택
+	
+	public Spark() { }
+
+	// alt + shift + s -> o  // 셀렉트박스에 아래 내용으로.
+	public Spark(int wheel, int seat, String fuel, double discountOffer) {
+		super(wheel, seat, fuel);
+		this.discountOffer = discountOffer;
+	}
+
+	
+	//getter/setter
+	public double getDiscountOffer() {
+		return discountOffer;
+	}
+
+	public void setDiscountOffer(double discountOffer) {
+		this.discountOffer = discountOffer;
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + " / 할인혜택 : " + discountOffer;
+	}
+	
+	
+	
+	public void dc() {
+		System.out.println( discountOffer * 100 + "% 할인됩니다.");
+	}
+
+}
