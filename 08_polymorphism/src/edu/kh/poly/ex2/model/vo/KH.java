7ef7e8031b3b884b971b7be082313b867adb30d6
luejenie <luejenie@gmail.com>
2022-08-24 12:51:33 +0900

    20220824 인터페이스

---
+++ 08_polymorphism/src/edu/kh/poly/ex2/model/vo/KH.java
@@ -0,0 +1,17 @@
+package edu.kh.poly.ex2.model.vo;
+
+public interface KH {  // 인터페이스
+	
+	// 필드(public static final)
+	/*public static final*/ String ADDRESS = "서울시 중구 남대문로 20";
+	
+	//_이클립스에 기울어진 표시는 static에 있는 걸 의미
+	//_ 위에서 (public static final)을 주석처리해도 ADDRESS는 기울어진 의미. 없어도 static에 포함되는 걸 의미
+	// 암묵적(묵시적)
+	
+	// 메서드(public abstract)
+	/*public abstract*/ void lesson();  // 수업
+
+	
+	
+}
