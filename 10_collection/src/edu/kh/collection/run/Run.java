57eacf4b04b7e727a26bc259a566640497648fd6
luejenie <luejenie@gmail.com>
2022-08-25 12:51:18 +0900

    20220825 컬렉션

---
+++ 10_collection/src/edu/kh/collection/run/Run.java
@@ -0,0 +1,19 @@
+package edu.kh.collection.run;
+
+import edu.kh.collection.model.service.ListService;
+import edu.kh.collection.view.StudentView;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+		ListService service1 = new ListService();
+		
+//		service1.ex1();
+		
+		
+		new StudentView().displayMenu(); // 객체 1회성 사용 (만들자마자 메서드만 호출하고 없어져라) 
+		
+		
+	}
+}
