62083d30f2b742c0554e85b8f02beadd250a4107
luejenie <luejenie@gmail.com>
2022-09-01 17:51:00 +0900

    20220901 EmploymentManagement
    선생님 코드 따라 써보는 중
    인터페이스, 상속 사용

---
+++ EmployeeManagement/src/edu/kh/emp/run/EmployeeRun.java
@@ -0,0 +1,12 @@
+package edu.kh.emp.run;
+
+import edu.kh.emp.view.EmployeeViewImpl;
+
+public class EmployeeRun {
+	public static void main(String[] args) {
+		
+		new EmployeeViewImpl().displayMenu();
+		
+	}
+
+}
---
+++ EmployeeManagement/src/edu/kh/emp/view/EmployeeViewImpl.java
@@ -0,0 +1,12 @@
+package edu.kh.emp.view;
+
+import java.util.Scanner;
+
+public class EmployeeViewImpl implements EmployeeView {
+	
+	private Scanner sc = new Scanner(System.in);
+	
+	
+	
+
+}
